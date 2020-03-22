7870eb9f538d1a28359a08f1ea36a9525381ed83
Usman <usman@usmans-mbp.home>
2020-03-22 15:57:28 +0500

    Uploaded project.

---
+++ CrowdCast/Source/Login/CCLoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  CCLoginVC.swift
+//  CrowdCast
+//
+//  Created by Usman on 22/03/2020.
+//  Copyright © 2020 Usman Nazir. All rights reserved.
+//
+
+import UIKit
+
+class CCLoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
