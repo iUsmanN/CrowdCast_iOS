9bcb27547a773bb4b928075cbeeaccdeb418ec3b
Usman <usman@usmans-mbp.home>
2020-03-23 16:02:06 +0500

    Adds Sign Up via email.

--- CrowdCast/Source/SignUp/CCSignUpVC.swift
+++ CrowdCast/Source/SignUp/CCSignUpVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CCSignUpVC: UIViewController {
 
@@ -14,5 +15,29 @@ class CCSignUpVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func signUpPressed(_ sender: Any) {
+        signUp_Email(email: "abc@abc.com", password: "abcabc")
+    }
+}
+
+extension CCSignUpVC {
+    
+    func signUp_Email(email: String, password: String){
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self](result, error) in
+            guard error == nil else { self?.signUpFailed(error: error); return }
+            self?.signUpSuccess(result: result)
+        }
+    }
+}
+
+extension CCSignUpVC {
+    
+    func signUpFailed(error: Error?){
+        print("sign up failed.")
+    }
+    
+    func signUpSuccess(result: AuthDataResult?){
+        print("signed up successfully.")
     }
 }
