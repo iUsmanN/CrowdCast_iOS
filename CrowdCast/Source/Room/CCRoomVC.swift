b0a35999c66c1fc4b0aeb35521f80cf574b97bea
Usman <usman@usmans-mbp.home>
2020-03-29 14:48:16 +0500

    Adds multiple participants = 3

--- CrowdCast/Source/Room/CCRoomVC.swift
+++ CrowdCast/Source/Room/CCRoomVC.swift
@@ -26,9 +26,11 @@ class CCRoomVC: UIViewController {
     var localVideoTrack : LocalVideoTrack?
     
     
-    var twilioAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlLTE1ODU0MDM1ODIiLCJpc3MiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlIiwic3ViIjoiQUM4OTRhZWJhMTZkZjllY2Q4OGYyMzg4NDg0MWU0NTk2ZCIsImV4cCI6MTU4NTQwNzE4MiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoidGVzdDEiLCJ2aWRlbyI6e319fQ.nS8-Dj-Tox44_hxtAGUPfZDR_KLyWnSOWB0p4qNmq54"
+    var twilioAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlLTE1ODU0Njc1MzYiLCJpc3MiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlIiwic3ViIjoiQUM4OTRhZWJhMTZkZjllY2Q4OGYyMzg4NDg0MWU0NTk2ZCIsImV4cCI6MTU4NTQ3MTEzNiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoidGVzdDEiLCJ2aWRlbyI6e319fQ._o0vDP3v261eMPsi2PUukg0SnRLA94jiGRKdbbQb0mM"
     
-    var twilioAccessToken2 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlLTE1ODU0MDM2NzEiLCJpc3MiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlIiwic3ViIjoiQUM4OTRhZWJhMTZkZjllY2Q4OGYyMzg4NDg0MWU0NTk2ZCIsImV4cCI6MTU4NTQwNzI3MSwiZ3JhbnRzIjp7ImlkZW50aXR5IjoidGVzdDIiLCJ2aWRlbyI6e319fQ.uuJ2frvVUWowwGKvECAfYJ2QhgAPf_i4cDiKG7EVPQw"
+    var twilioAccessToken2 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlLTE1ODU0Njc1NTYiLCJpc3MiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlIiwic3ViIjoiQUM4OTRhZWJhMTZkZjllY2Q4OGYyMzg4NDg0MWU0NTk2ZCIsImV4cCI6MTU4NTQ3MTE1NiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoidGVzdDIiLCJ2aWRlbyI6e319fQ.tGJyGz8V0n9-73d_CRBW7RCF1RAw0-8jeCKjkqm-Ve8"
+    
+    var twilioAccessToken3 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlLTE1ODU0Njc1NzIiLCJpc3MiOiJTSzcwNmE1YjVmMmM2NTlhYzA5ZWZhMmM1N2QyMTI1NTRlIiwic3ViIjoiQUM4OTRhZWJhMTZkZjllY2Q4OGYyMzg4NDg0MWU0NTk2ZCIsImV4cCI6MTU4NTQ3MTE3MiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoidGVzdDMiLCJ2aWRlbyI6e319fQ.BlYZYlERZKjf3dHxzuK5_A2_qSbCgSRQR1ra3-mNk2Q"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +52,7 @@ class CCRoomVC: UIViewController {
 extension CCRoomVC {
     
     func createARoom() {
-        let connectOptions = ConnectOptions(token: twilioAccessToken2) { (builder) in
+        let connectOptions = ConnectOptions(token: twilioAccessToken3) { (builder) in
             builder.roomName = "my-room"
             
             if let audioTrack = self.localAudioTrack {
@@ -84,7 +86,7 @@ extension CCRoomVC : RoomDelegate {
     }
     
     func roomDidFailToConnect(room: Room, error: Error) {
-        print("roomDidFailToConnect")
+        print("roomDidFailToConnect -> \(error)")
     }
     
     func roomIsReconnecting(room: Room, error: Error) {
@@ -113,7 +115,7 @@ extension CCRoomVC : CameraSourceDelegate, RemoteParticipantDelegate {
         
         print("Participant \(participant.identity) added a video track.")
         
-        if let remoteView = VideoView.init(frame: self.otherview.bounds,
+        if let remoteView = VideoView.init(frame: self.view.bounds,
                                            delegate:self) {
             
             videoTrack.addRenderer(remoteView)
