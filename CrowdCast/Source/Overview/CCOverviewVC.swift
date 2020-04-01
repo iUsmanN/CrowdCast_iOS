8ef3b473db47afcea4136897614fd1cb3a39e32e
Usman <usman@usmans-mbp.home>
2020-04-01 21:42:38 +0500

    Added cards

--- CrowdCast/Source/Overview/CCOverviewVC.swift
+++ CrowdCast/Source/Overview/CCOverviewVC.swift
@@ -14,6 +14,7 @@ class CCOverviewVC: UIViewController {
     @IBOutlet weak var greetingView: CCNavBar!
     @IBOutlet weak var greetingHeight: NSLayoutConstraint!
     
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +23,18 @@ class CCOverviewVC: UIViewController {
     
     func setupView(){
         setupNavBar(navigationItem: navigationItem, title: "Overview", profileAction: #selector(openSettings))
+        setupTableView()
     }
 }
 
 extension CCOverviewVC : CCNavbarProtocol {
     
+    func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        let cardNib = UINib(nibName: "CCCardTVCTableViewCell", bundle: nil)
+        tableView.register(cardNib, forCellReuseIdentifier: "CCCardTVCTableViewCell")
+    }
 }
 
 extension CCOverviewVC {
@@ -34,4 +42,32 @@ extension CCOverviewVC {
     @objc func openSettings(){
         print("Open Settings")
     }
+}
+
+extension CCOverviewVC : UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CCCardTVCTableViewCell", for: indexPath) as? CCCardTVCTableViewCell else { return UITableViewCell()}
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    
 }
