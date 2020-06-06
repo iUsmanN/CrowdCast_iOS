444284833aea8b7b8d0fcba0826ac8cf7cdfc4f3
Usman <usman@usmans-mbp.home>
2020-06-06 23:28:09 +0500

    WIP

--- CrowdCast/Services/CCSharelinkService.swift
+++ CrowdCast/Services/CCSharelinkService.swift
@@ -7,12 +7,22 @@
 //
 
 import Foundation
+import FirebaseDynamicLinks
 
 protocol CCSharelinkService {}
 
 extension CCSharelinkService {
     
     func generateSharelink(id: String, type: CCJoinableType, completion: @escaping (Result<String, CCError>)->()){
+        
+        guard let link = URL(string: "https://crowdcast.page.link") else { return }
+        let dynamicLinksDomainURIPrefix = "https://crowdcast.page.link/link"
+        let linkBuilder = DynamicLinkComponents(link: link, domainURIPrefix: dynamicLinksDomainURIPrefix)
+        linkBuilder?.iOSParameters = DynamicLinkIOSParameters(bundleID: "com.example.ios")
+        linkBuilder?.androidParameters = DynamicLinkAndroidParameters(packageName: "com.example.android")
+
+        guard let longDynamicLink = linkBuilder?.url else { return }
+        print("The long URL is: \(longDynamicLink)")
         
     }
 }
